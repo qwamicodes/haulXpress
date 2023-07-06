75a7a60af334121840884fb93fa79940f281fb70
qwamicodes <qwaedjekum@gmail.com>
2023-07-06 21:04:43 +0000

    added store config -added apploading component -added navigation point

--- App.tsx
+++ App.tsx
@@ -1,20 +1,19 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { SafeAreaProvider } from "react-native-safe-area-context";
+import { Provider } from "react-redux";
+
+import store from "./store";
+import Navigation from "./navigation";
+
+import { AppLoading } from "./components/AppLoading";
 
 export default function App() {
   return (
-    <View style={styles.container}>
-      <Text>Open up App.tsx to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
+    <SafeAreaProvider>
+      <Provider store={store}>
+        <AppLoading>
+          <Navigation />
+        </AppLoading>
+      </Provider>
+    </SafeAreaProvider>
   );
 }
-
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
