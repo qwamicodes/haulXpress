b749df04ffcf66d95e78c1e95c8c514b49623340
qwamicodes <qwaedjekum@gmail.com>
2023-07-06 20:53:04 +0000

    added screen template to use for other screens

---
+++ screens/splash/index.tsx
@@ -0,0 +1,16 @@
+import { StyleSheet, Text, View } from "react-native";
+import React from "react";
+
+import ScreenTemplate from "../template/ScreenTemplate";
+
+const SplashScreen = () => {
+  return (
+    <ScreenTemplate>
+      <Text>SplashScreen</Text>
+    </ScreenTemplate>
+  );
+};
+
+export default SplashScreen;
+
+const styles = StyleSheet.create({});
