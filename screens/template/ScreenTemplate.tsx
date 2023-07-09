a274985ee0ada3cbe6b5a56dc4bcb18b1bbda96b
qwamicodes <qwaedjekum@gmail.com>
2023-07-09 04:47:00 +0000

    maded neccesary changes

--- screens/template/ScreenTemplate.tsx
+++ screens/template/ScreenTemplate.tsx
@@ -1,9 +1,16 @@
 import React, { ReactNode } from "react";
 import { SafeAreaView } from "react-native-safe-area-context";
 import { screenStyle } from "../../constants";
+import { ViewStyle } from "react-native";
 
-const ScreenTemplate = ({ children }: { children: ReactNode }) => {
-  return <SafeAreaView style={[screenStyle]}>{children}</SafeAreaView>;
+const ScreenTemplate = ({
+  children,
+  styles,
+}: {
+  children: ReactNode;
+  styles?: ViewStyle;
+}) => {
+  return <SafeAreaView style={[screenStyle, styles]}>{children}</SafeAreaView>;
 };
 
 export default ScreenTemplate;
