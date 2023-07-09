c6b8ba908bbd129c84a0b7e47d0c956f0e6aa385
qwamicodes <qwaedjekum@gmail.com>
2023-07-09 19:11:06 +0000

    added the authentication screens

--- navigation/MainNavigator.tsx
+++ navigation/MainNavigator.tsx
@@ -2,8 +2,10 @@ import React from "react";
 import { createNativeStackNavigator } from "@react-navigation/native-stack";
 
 import { RootStackParamList } from "../types";
-import SplashScreen from "../screens/splash";
+
 import Walkthrough from "../features/Walkthrough";
+import Login from "../features/Authentication/Login";
+import Register from "../features/Authentication/Register";
 
 const MainNavigator = () => {
   const { Navigator, Group, Screen } =
@@ -16,13 +18,27 @@ const MainNavigator = () => {
         headerShown: false,
       }}
     >
+      {/* <Group
+        key="authenticated"
+        screenOptions={{
+          headerShown: false,
+        }}
+      ></Group> */}
+      <Group
+        key="unathenticated"
+        screenOptions={{
+          headerShown: false,
+        }}
+      >
+        <Screen name="Login" component={Login} />
+        <Screen name="Register" component={Register} />
+      </Group>
       <Group
         key="others"
         screenOptions={{
           headerShown: false,
         }}
       >
-        {/* <Screen name="SplashScreen" component={SplashScreen} /> */}
         <Screen name="Walkthrough" component={Walkthrough} />
       </Group>
     </Navigator>
