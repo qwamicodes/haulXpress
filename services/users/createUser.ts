20640907830cc9dac760a1fc23eb6ff4032bb7e4
qwamicodes <qwaedjekum@gmail.com>
2023-07-10 17:15:53 +0000

    added api functions to use for authentication

---
+++ services/users/createUser.ts
@@ -0,0 +1,26 @@
+import { NativeStackNavigationProp } from "@react-navigation/native-stack";
+import { createUserWithEmailAndPassword } from "firebase/auth";
+
+import { auth } from "../../confg/firebase";
+import { AppDispatch } from "../../store";
+import { RootStackParamList } from "../../types";
+import { togglePending } from "../../store/slices/authSlice";
+
+const createUser =
+  (
+    email: string,
+    password: string,
+    navigate: NativeStackNavigationProp<RootStackParamList>
+  ) =>
+  async (dispatch: AppDispatch) => {
+    dispatch(togglePending());
+
+    await createUserWithEmailAndPassword(auth, email, password)
+      .then((user) => {
+        console.log(user);
+      })
+      .catch((error) => console.log(error.message))
+      .finally(() => dispatch(togglePending()));
+  };
+
+export default createUser;
---
+++ services/users/signinUser.ts
@@ -0,0 +1,5 @@
+import auth from "@react-native-firebase/auth";
+
+const signinUser = (email: string, password: string) => {};
+
+export default signinUser;
