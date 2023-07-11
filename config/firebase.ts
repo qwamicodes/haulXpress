cba2f96f419c3405e99a6677588be3b1b496059f
qwamicodes <qwaedjekum@gmail.com>
2023-07-11 15:29:58 +0000

    set environment variables

--- config/firebase.ts
+++ config/firebase.ts
@@ -10,14 +10,24 @@ import { getStorage } from "firebase/storage";
 // import {...} from "firebase/storage";
 
 // Initialize Firebase
+const {
+  RN_APP_API_KEY,
+  RN_APP_AUTH_DOMAIN,
+  RN_APP_PROJECT_ID,
+  RN_APP_STORAGE_BUCKET,
+  RN_APP_MESSAGING_SENDER_ID,
+  RN_APP_APP_ID,
+  RN_APP_MEASUREMENT_ID,
+} = process.env;
+
 const firebaseConfig = {
-  apiKey: "AIzaSyBwijOiYr2D8jjoYRqDgJ8y1iUMvISXa3k",
-  authDomain: "haulxpress-c3ab7.firebaseapp.com",
-  projectId: "haulxpress-c3ab7",
-  storageBucket: "haulxpress-c3ab7.appspot.com",
-  messagingSenderId: "977322094900",
-  appId: "1:977322094900:web:c78c8ad8cd46b74ab3f561",
-  measurementId: "G-L9G7C141E4",
+  apiKey: RN_APP_API_KEY,
+  authDomain: RN_APP_AUTH_DOMAIN,
+  projectId: RN_APP_PROJECT_ID,
+  storageBucket: RN_APP_STORAGE_BUCKET,
+  messagingSenderId: RN_APP_MEASUREMENT_ID,
+  appId: RN_APP_APP_ID,
+  measurementId: RN_APP_MEASUREMENT_ID,
 };
 
 const app = initializeApp(firebaseConfig);
