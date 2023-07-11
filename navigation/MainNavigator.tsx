42640651ba27bd1dee205bfaed728732269ca241
qwamicodes <qwaedjekum@gmail.com>
2023-07-11 01:52:53 +0000

    added the new screens into the stack

--- navigation/MainNavigator.tsx
+++ navigation/MainNavigator.tsx
@@ -2,37 +2,50 @@ import React from "react";
 import { createNativeStackNavigator } from "@react-navigation/native-stack";
 
 import { RootStackParamList } from "../types";
+import { useAppSelector } from "../hooks";
 
 import Walkthrough from "../features/Walkthrough";
 import Login from "../features/Authentication/Login";
 import Register from "../features/Authentication/Register";
+import Onboarding from "../features/Authentication/Onboarding";
+import Home from "../screens/Authenticated/Home";
+import Hauler from "../features/Hauler";
 
 const MainNavigator = () => {
+  const { isAuthenticated } = useAppSelector((state) => state.auth);
+
   const { Navigator, Group, Screen } =
     createNativeStackNavigator<RootStackParamList>();
 
   return (
     <Navigator
-      initialRouteName="Walkthrough"
+      initialRouteName={isAuthenticated ? "Hauler" : "Walkthrough"}
       screenOptions={{
         headerShown: false,
       }}
     >
-      {/* <Group
-        key="authenticated"
-        screenOptions={{
-          headerShown: false,
-        }}
-      ></Group> */}
-      <Group
-        key="unathenticated"
-        screenOptions={{
-          headerShown: false,
-        }}
-      >
-        <Screen name="Login" component={Login} />
-        <Screen name="Register" component={Register} />
-      </Group>
+      {isAuthenticated ? (
+        <Group
+          key="authenticated"
+          screenOptions={{
+            headerShown: false,
+          }}
+        >
+          <Screen name="Onboarding" component={Onboarding} />
+          <Screen name="Home" component={Home} />
+          <Screen name="Hauler" component={Hauler} />
+        </Group>
+      ) : (
+        <Group
+          key="unathenticated"
+          screenOptions={{
+            headerShown: false,
+          }}
+        >
+          <Screen name="Login" component={Login} />
+          <Screen name="Register" component={Register} />
+        </Group>
+      )}
       <Group
         key="others"
         screenOptions={{
