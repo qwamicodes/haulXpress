fe9ab8093e0492ca2626b1852c2bba9f603ccc2d
qwamicodes <qwaedjekum@gmail.com>
2023-07-11 01:52:04 +0000

    added some more strings for the alerts messages

--- constants/strings/index.ts
+++ constants/strings/index.ts
@@ -1,9 +1,24 @@
 export const authPasswordLength = 6;
 export const emailRegex = /^[^\s@]+@[^\s@]+\.[^\s@]+$/;
+export const phoneNumberRegex = /^\d+$/;
 
-export const ERRORS = {
+export const STRINGS = {
   userNotFound: "User not found!",
   invalidPasswordLength: `Password length must be greater than ${authPasswordLength}`,
-  invalidEmail: "Invalid Email",
+  invalidEmail: "Invalid email address",
+  invalidPassword: "Invalid password",
+  invalidPhoneNumber: "Invalid phone number",
   invalidEmailAndPassword: "Email and password are not valid",
+  successLogin: "Login successful",
+  successLogout: "Logout successful",
+  successRegister: "Registration successful",
+  successResetPassword: "Reset password successful",
+  successUpdatePassword: "Update password successful",
+  successDeleteUser: "Delete user successful",
+  successProfileUpdate: "Profile update successful",
+  wrongPassword: "Wrong password",
+  somethingHappened: "Something unexpectedly happened",
+  userDisabled: "User is disabled",
+  fillfields: "Fill in all the fields",
+  userDataNotFound: "User data not found",
 };
