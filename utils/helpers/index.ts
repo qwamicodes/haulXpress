06aa29a69a403950549c2e53ebdf1b5853dc3101
qwamicodes <qwaedjekum@gmail.com>
2023-07-11 01:52:37 +0000

    added another fucntion to handle the check of onboarding fields

--- utils/helpers/index.ts
+++ utils/helpers/index.ts
@@ -1,20 +1,48 @@
-import { ERRORS, authPasswordLength, emailRegex } from "../../constants";
-import { authProps } from "../../features/Authentication/reducer/authReducer";
+import {
+  STRINGS,
+  authPasswordLength,
+  emailRegex,
+  phoneNumberRegex,
+} from "../../constants";
+import { UserData, validationType } from "../../types";
 
 export const checkAuthValues = ({
   email,
-  password,
-}: authProps): { message: string; valid: boolean } => {
+  password = "password",
+}: {
+  email: string;
+  password?: string;
+}): validationType => {
   //check if email and password are valid
   if (!(email && password))
-    return { valid: false, message: ERRORS.invalidEmailAndPassword };
+    return { valid: false, message: STRINGS.fillfields };
 
+  //check if email is valid with the right format
   if (!emailRegex.test(email))
-    return { valid: false, message: ERRORS.invalidEmail };
+    return { valid: false, message: STRINGS.invalidEmail };
 
   //check if password length is greater or equal than max length of authPasswordLength
   if (password.length <= authPasswordLength)
-    return { valid: false, message: ERRORS.invalidPasswordLength };
+    return { valid: false, message: STRINGS.invalidPasswordLength };
+
+  return { message: "", valid: true };
+};
+
+export const checkOnboardingValues = ({
+  displayName,
+  occupation,
+  phoneNumber,
+}: Pick<
+  UserData,
+  "displayName" | "phoneNumber" | "occupation"
+>): validationType => {
+  //check if form fields are valid
+  if (!(displayName && occupation && phoneNumber))
+    return { valid: false, message: STRINGS.fillfields };
+
+  //check if phone number is valid with the right format
+  if (!phoneNumberRegex.test(phoneNumber))
+    return { valid: false, message: STRINGS.invalidPhoneNumber };
 
   return { message: "", valid: true };
 };
