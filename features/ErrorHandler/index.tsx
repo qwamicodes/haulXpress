e09a3bbbf940ee8e55a2453f91d5460308e1dc81
qwamicodes <qwaedjekum@gmail.com>
2023-07-12 23:28:23 +0000

    removed console.log

--- features/ErrorHandler/index.tsx
+++ features/ErrorHandler/index.tsx
@@ -1,7 +1,6 @@
 import { STRINGS } from "../../constants";
 
 const ErrorHandler = (code: string) => {
-  console.log(code);
   switch (code) {
     case "auth/user-not-found":
       alert(STRINGS.userNotFound);
