6767a372af728fca32b5a4f4b6e237992f68e9d1
qwamicodes <qwaedjekum@gmail.com>
2023-07-25 05:27:26 +0000

    added payment method types

--- types/data/index.ts
+++ types/data/index.ts
@@ -3,3 +3,10 @@ export interface IWalkThroughItem {
   title: string;
   description: string;
 }
+
+export type paymentType = "momo" | "cash";
+
+export type paymentMethod = {
+  name: string;
+  value: paymentType;
+};
