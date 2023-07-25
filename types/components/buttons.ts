91236cf1e5880fc737af4c8e939464f68e797126
qwamicodes <qwaedjekum@gmail.com>
2023-07-25 05:26:16 +0000

    included type variants to the button types

--- types/components/buttons.ts
+++ types/components/buttons.ts
@@ -1,10 +1,13 @@
 import { TouchableOpacityProps } from "react-native";
 import { iconType } from "./icon";
 
+type buttonTypes = "primary" | "secondary" | "success";
+
 export interface IButton extends TouchableOpacityProps {
   buttonText: string;
   icon: boolean;
   iconType?: iconType;
   lightIcon?: boolean;
   iconColor?: string;
+  type?: buttonTypes;
 }
