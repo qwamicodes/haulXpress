4368d59af2e26f28c31b4b729fef3497f8696e52
qwamicodes <qwaedjekum@gmail.com>
2023-07-29 17:50:08 +0000

    removed hook

--- hooks/index.ts
+++ hooks/index.ts
@@ -1,5 +1,4 @@
 export { useAppDispatch, useAppSelector } from "./store";
 export { default as useCachedResources } from "./useCachedResources";
 export { default as useOauthentication } from "./authentication/useOauthentication";
-export { default as getCurrentLocation } from "./location/getCurrentLocation";
 export * from "./navigation";
