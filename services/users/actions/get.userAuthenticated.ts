2114e9d83c88f6d081ba196c8a1121db12820563
qwamicodes <qwaedjekum@gmail.com>
2023-08-17 18:21:08 +0000

    service action to check user logged in google session

---
+++ services/users/actions/get.userAuthenticated.ts
@@ -0,0 +1,42 @@
+import { onAuthStateChanged } from "firebase/auth";
+
+import { AppDispatch } from "../../../store";
+import { auth } from "../../../config/firebase";
+import {
+  loginUserToState,
+  togglePending,
+} from "../../../store/slices/authSlice";
+import getUserDataFromStore from "./get.UserDataFromStore";
+import { Dispatch, SetStateAction } from "react";
+
+const getUserAuthenticated =
+  (setIsRequestCompleted: Dispatch<SetStateAction<boolean>>) =>
+  async (dispatch: AppDispatch) => {
+    dispatch(togglePending(true));
+
+    try {
+      onAuthStateChanged(auth, async (userCredential) => {
+        if (userCredential) {
+          const user = {
+            email: userCredential.email as string,
+            emailVerified: userCredential.emailVerified,
+            userId: userCredential.uid,
+          };
+
+          const userData = await dispatch(
+            getUserDataFromStore(user.userId)
+          ).finally(() => setIsRequestCompleted(true));
+
+          if (userData) {
+            dispatch(loginUserToState({ ...user, ...userData }));
+          }
+        }
+      });
+    } catch (e) {
+      console.error(e);
+    } finally {
+      dispatch(togglePending(false));
+    }
+  };
+
+export default getUserAuthenticated;
