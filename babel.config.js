061f3399a9d6a8ea07351cb0da15c161562bdead
qwamicodes <qwaedjekum@gmail.com>
2026-09-16 13:32:48 +0000

    refactor(app): update the expo

--- babel.config.js
+++ babel.config.js
@@ -3,7 +3,6 @@ module.exports = function (api) {
   return {
     presets: ['babel-preset-expo'],
     plugins: [
-      'react-native-reanimated/plugin',
       [
         'module:react-native-dotenv',
         {
@@ -12,6 +11,7 @@ module.exports = function (api) {
           path: '.env',
         },
       ],
+      'react-native-reanimated/plugin',
     ],
   };
 };
